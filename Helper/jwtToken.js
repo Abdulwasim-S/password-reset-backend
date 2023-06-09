07296a1242517840b4f3e165b2d1809d2d2e238a
Abdulwasim-S <abdulwasimsmech@gmail.com>
2023-06-09 12:14:24 +0530

    undo

--- Helper/jwtToken.js
+++ Helper/jwtToken.js
@@ -2,6 +2,6 @@ import jwt from "jsonwebtoken";
 import {} from 'dotenv/config.js'
 
 export async function generateToken(value){
-    const token=jwt.sign(value,process.env.SECRET_KEY,expiresIn = '30d')
+    const token=jwt.sign(value,process.env.SECRET_KEY)
     return token
 }
